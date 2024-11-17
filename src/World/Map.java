5fd544ed51b104c01f0bf9662083559e9d4edc2b
moiz <moizahmad7777@gmail.com>
2024-11-16 22:30:49 -0500

    Updated map reading

--- src/World/Map.java
+++ src/World/Map.java
@@ -1,9 +1,66 @@
 package World;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Map {
 
-    JSONParser parser = new JSONParser();
+    private String mapDirectory;
+    JSONParser parser;
+    FileReader reader;
 
+    private int mapWidth, mapHeight;
+    Tile[][] baseMapTiles;
+    Tile[][] obstacleMapTiles;
+
+
+    Map(String mapDirectory) {
+        this.mapDirectory = mapDirectory;
+    }
+
+    void getMap() throws IOException, ParseException {
+        try {
+            parser = new JSONParser();
+            reader = new FileReader(mapDirectory);
+        } catch(FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+        // Parse the JSON file
+        JSONObject jsonMapObject = (JSONObject) parser.parse(reader);
+
+        mapWidth = Integer.parseInt(jsonMapObject.get("mapWidth").toString());
+        mapHeight = Integer.parseInt(jsonMapObject.get("mapHeight").toString());
+
+        baseMapTiles = new Tile[mapWidth][mapHeight];
+        obstacleMapTiles = new Tile[mapWidth][mapHeight];
+
+        JSONArray layers = (JSONArray) jsonMapObject.get("layers");
+
+        for(int i = 0; i < layers.size(); i++) {
+            JSONObject currentLayer = (JSONObject) layers.get(i);
+            JSONArray dataArray = (JSONArray) currentLayer.get("data");
+
+            for(int j = 0; j < dataArray.size(); j++) {
+
+                for(int m = 0; m < mapHeight; m++) {
+                    for(int n = 0; n < mapWidth; n++) {
+                        if(i == 0) {
+                            baseMapTiles[m][n] = new Tile(m, n, Integer.parseInt(dataArray.get(j).toString()));
+                        } else if(i == 1) {
+                            obstacleMapTiles[m][n] = new Tile(m, n, Integer.parseInt(dataArray.get(j).toString()));
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
 }
--- src/World/Tile.java
+++ src/World/Tile.java
@@ -5,16 +5,73 @@ public class Tile {
     public static final int tileMultipler = 4;
     public static final int tileSize = startingTileSize*tileMultipler;
 
-    private int tileWidth, tileHeight;
-    private int worldXPos, worldtileYPos;
+    private int value;
+    private int row, col;
+    private int worldXPos, worldYPos;
     private int screenXPos, screenYPos;
 
     boolean isCollidable;
 
+    Tile(int row, int col, int value) {
+        this.row = row;
+        this.col = col;
+        this.value = value;
+
+        setWorldX();
+        setWorldY();
+    }
+
+    private void setWorldX() {
+        worldXPos = col * tileSize;
+    }
+
+    private void setWorldY() {
+        worldYPos = row * tileSize;
+    }
+
     // CURRENT IMAGE
     // IMAGE LIST
 
-
     //ANIMATION METHOD
 
+    // Getters and Setters
+    public int getWorldXPos() {
+        return worldXPos;
+    }
+
+    public void setWorldXPos(int worldXPos) {
+        this.worldXPos = worldXPos;
+    }
+
+    public int getWorldYPos() {
+        return worldYPos;
+    }
+
+    public void setWorldYPos(int worldYPos) {
+        this.worldYPos = worldYPos;
+    }
+
+    public int getScreenXPos() {
+        return screenXPos;
+    }
+
+    public void setScreenXPos(int screenXPos) {
+        this.screenXPos = screenXPos;
+    }
+
+    public int getScreenYPos() {
+        return screenYPos;
+    }
+
+    public void setScreenYPos(int screenYPos) {
+        this.screenYPos = screenYPos;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
