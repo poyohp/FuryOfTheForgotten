5f81049d25c62cbfb957cc42e5280c56d16d01f4
aliceliu391 <alicehaichenliu@gmail.com>
2024-12-01 12:57:46 -0500

    Centered health drawing

--- src/Entities/Entity.java
+++ src/Entities/Entity.java
@@ -52,7 +52,7 @@ public abstract class Entity {
     }
 
     public void drawHealth(Graphics2D g2) {
-        g2.drawString(String.format("%.2f", this.health), (int)this.screenX, (int)(this.screenY - 20));
+        g2.drawString(String.format("%.2f", this.health), (int)this.screenX + this.getWidth()/4, (int)(this.screenY - 20));
     }
 
     public double getHealth() {
