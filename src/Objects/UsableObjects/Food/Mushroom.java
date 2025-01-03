0fbe356e19d2419b78fd9ef5effe261f46f0c6fa
moiz <moizahmad7777@gmail.com>
2025-01-03 18:42:31 -0500

    added random effects with mushroom

--- src/Objects/UsableObjects/Food/Mushroom.java
+++ src/Objects/UsableObjects/Food/Mushroom.java
@@ -21,6 +21,64 @@ public class Mushroom extends UsableObject {
 
     @Override
     public void isUsed(Player player) {
-        //RANDOM EFFECT
+        if (GamePanel.random.nextInt(2) == 0) {
+            //50 percent chance to activate poison and nothing else!
+            player.healthHandler.activatedPoison();
+        } else {
+            int num = GamePanel.random.nextInt(3);
+            int goodEffect = GamePanel.random.nextInt(3);
+            int badEffect = GamePanel.random.nextInt(2);
+            if(num == 0) {
+                //17 percent chance of 2 good effects!
+                if(goodEffect > 0) {
+                    player.speedBoostUsed();
+                    player.healthHandler.activatedShields(player.healthHandler.maxShields/2.0);
+                } else {
+                    player.healthHandler.activatedShields(player.healthHandler.maxShields/2.0);
+                    player.healthHandler.heal(2);
+                }
+            } else if(num == 1){
+                //17 percent chance of just 1 good effect
+                switch(goodEffect) {
+                    case 0:
+                        player.speedBoostUsed();
+                        break;
+                    case 1:
+                        player.damageBoostUsed();
+                        break;
+                    case 2:
+                        player.healthHandler.activatedShields(player.healthHandler.maxShields/2.0);
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                //17 percent chance of 1 good effect + 1 bad effect
+                switch(goodEffect) {
+                    case 0:
+                        player.damageBoostUsed();
+                        break;
+                    case 1:
+                        player.speedBoostUsed();
+                        break;
+                    case 2:
+                        player.healthHandler.activatedShields(player.healthHandler.maxShields/2.0);
+                        break;
+                    default:
+                        break;
+                }
+
+                switch(badEffect) {
+                    case 0:
+                        player.healthHandler.activatedPoison();
+                        break;
+                    case 1:
+                        player.isHit(2);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     }
 }
