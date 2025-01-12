e0bd95d946f8caaf8dced8aa1ef9b20edf848943
moiz <moizahmad7777@gmail.com>
2025-01-11 23:32:32 -0500

    fixed unnecessary delete

--- src/System/Panels/GamePanel.java
+++ src/System/Panels/GamePanel.java
@@ -17,6 +17,7 @@ import World.Tile;
 
 import javax.swing.JPanel;
 import java.awt.*;
+import java.util.Random;
 
 
 public class GamePanel extends JPanel implements Runnable{
@@ -48,6 +49,8 @@ public class GamePanel extends JPanel implements Runnable{
 
     public InstantKill ghost;
     public EternalSnail snail;
+
+    Random random = new Random();
 
     /**
      * Constructor for the GamePanel - initializes all objects and starts the game
