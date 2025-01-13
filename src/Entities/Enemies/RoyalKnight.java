1d99c09f2ec91bab4b26e43ae3139d10712473e3
aliceliu391 <alicehaichenliu@gmail.com>
2025-01-13 12:45:25 -0500

    Fixed knight attacking and pathfinding

--- src/Entities/Enemies/RoyalKnight.java
+++ src/Entities/Enemies/RoyalKnight.java
@@ -4,11 +4,13 @@ import Entities.Players.Player;
 import Handlers.Attacks.AttackHandler;
 import Handlers.ImageHandler;
 import Pathfinding.APathfinding;
+import Pathfinding.Node;
 import System.Panels.GamePanel;
 import World.Tile;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 public class RoyalKnight extends Enemy{
 
@@ -21,7 +23,6 @@ public class RoyalKnight extends Enemy{
     final int columnWidth = 96, rowHeight = 97;
     BufferedImage knight = ImageHandler.loadImage("Assets/Entities/Enemies/Boss/Elite Knight Sprite Sheet.png");
     BufferedImage swing = ImageHandler.loadImage("Assets/Entities/Enemies/Boss/Attack Sprite Sheet.png");
-
 
     /**
      * Enemy that follows player
@@ -60,13 +61,7 @@ public class RoyalKnight extends Enemy{
         setScreenPosition();
 
         hitbox.update(this);
-        if (!attacking) {
-            move();
-        }
-        checkInRange();
-        if (inRange) {
-            attack(a);
-        }
+        if (!attacking) move();
     }
 
     @Override
@@ -176,26 +171,70 @@ public class RoyalKnight extends Enemy{
 
     @Override
     public void move() {
-        double centerX = worldX + (double) getWidth() /2;
-        double centerY = worldY + (double) getHeight() /2;
-        double entityX = entityToFollow.worldX + (double) entityToFollow.getWidth() /2;
-        double entityY = entityToFollow.worldY + (double) entityToFollow.getHeight() /2;
-        double angle;
 
-        if (entityX > centerX && entityY > centerY) {
-            angle = (2*Math.PI) - Math.atan(((entityY - centerY) / (entityX - centerX)));
-        } else if (entityX > centerX && entityY < centerY) {
-            angle = Math.atan((centerY - entityY) / (entityX - centerX));
-        } else if (entityX < centerX && entityY > centerY) {
-            angle = Math.PI + Math.atan(((entityY - centerY) / (centerX - entityX)));
-        } else {
-            angle = Math.PI - Math.atan(((centerY - entityY) / (centerX - entityX)));
+        // If enemy is going to follow player
+        if (onPath) {
+
+            // Checks whether the knight is closer to the right side or left side of the player
+            double xLeftDistance = Math.abs(this.screenX - entityToFollow.entityLeft);
+            double xRightDistance = Math.abs(this.screenX - entityToFollow.entityRight);
+
+            int goalRow;
+            int goalCol;
+
+            if (xLeftDistance > xRightDistance) { // Go to right side of player
+                goalRow = (int) (entityToFollow.entityTop/ Tile.tileSize); //top row of the player
+                goalCol = (int) (entityToFollow.entityRight/Tile.tileSize); //right side of the player
+            } else { // Go to the left side of player
+                goalRow = (int) (entityToFollow.entityTop/ Tile.tileSize); //top row of the player
+                goalCol = (int) (entityToFollow.entityLeft/Tile.tileSize); //left side of the player
+            }
+
+            searchPath(goalRow, goalCol);
         }
+    }
 
-        System.out.println(angle);
+    /**
+     * Searches for a path to the player
+     * @param goalRow Player's row
+     * @param goalCol Player's column
+     */
+    public void searchPath(int goalRow, int goalCol) {
 
-        worldX += (int)(getSpeed() * Math.cos(angle));
-        worldY += (int)(-getSpeed() * Math.sin(angle));
+        int startRow = (int) (this.entityTop/Tile.tileSize); //top row of the enemy
+        int startCol = (int) (this.entityLeft/Tile.tileSize); //left row of the enemy
+
+        pathFinder.setNodes(tileset[startRow][startCol], tileset[goalRow][goalCol]);
+
+        // If a path is found, move towards the player
+        if (pathFinder.findPath()) {
+            ArrayList<Node> path = pathFinder.shortestPath; // List of tiles to go to
+
+            // Next tile to go to
+            double nextCol = path.get(0).col;
+            double nextRow = path.get(0).row;
+
+            // Next x and y position to go to (calculated with tile size)
+            double nextWorldX = nextCol * Tile.tileSize;
+            double nextWorldY = nextRow * Tile.tileSize;
+
+            // Ensures that enemy does not move when it is too close or too far from player
+            if (path.size() < 4 && startRow == goalRow) {
+
+
+                // Turn in direction of player, even if not moving toward them
+                if (worldX > nextWorldX) direction = 'l'; // left
+                else if (worldX < nextWorldX) direction = 'r'; // right
+
+                attack(a);
+                return; // Does not move to player
+            }
+
+            getNewPosition(nextWorldX, nextWorldY);
+
+        } else {
+            moveTowardPlayer();
+        }
     }
 
     @Override
