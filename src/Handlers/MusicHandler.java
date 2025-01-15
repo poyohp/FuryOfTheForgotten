3e7201cf9887934bc7844dd9926a2f9136d80366
aliceliu391 <alicehaichenliu@gmail.com>
2025-01-15 17:57:50 -0500

    Updated images with new buttons (for arcade machine)

--- src/Handlers/MusicHandler.java
+++ src/Handlers/MusicHandler.java
@@ -1,11 +1,8 @@
 package Handlers;
 
-import World.Level;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import java.io.InputStream;
 import java.net.URL;
 
 public class MusicHandler {
@@ -18,7 +15,7 @@ public class MusicHandler {
         soundList[0] = getClass().getResource("/Assets/Sound/Soundtrack1.wav");
         soundList[1] = getClass().getResource("/Assets/Sound/Soundtrack2.wav");
         soundList[2] = getClass().getResource("/Assets/Sound/Soundtrack3.wav");
-        soundList[3] = getClass().getResource("/Assets/Sound/Soundtrack4.wav");
+        soundList[3] = getClass().getResource("/Sound/Soundtrack4.wav");
         soundList[4] = getClass().getResource("/Assets/Sound/Soundtrack5.wav");
         soundList[5] = getClass().getResource("/Assets/Sound/BossSoundtrack2.wav");
 
